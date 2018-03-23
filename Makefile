d292c2c8e1bfa137205cabc61430784c23474e23
Wende Tan <twd2@163.com>
2018-03-23 17:56:53 +0800

    ex1: problem 1 report

---
+++ Makefile
@@ -0,0 +1,4 @@
+.PHONY: clean
+clean:
+	-rm ex1/*.exe ex1/*.out ex1/*.o
+	-rm ex2/*.exe ex2/*.out ex2/*.o
--- ex1/error.cpp
+++ ex1/error.cpp
@@ -6,7 +6,7 @@ int main()
 {
     int n = 0, sign = -1;
     float xn = 0, eps = 0.5e-4, ln2 = 0.693147190546;
-    while (std::abs(xn - ln2) > eps)
+    while (std::abs(xn - ln2) >= eps)
     {
         ++n;
         sign = -sign;
@@ -14,7 +14,7 @@ int main()
         std::cout << std::fixed << std::setprecision(20) << n << " " << xn << " " << (xn - ln2) << std::endl;
     }
     std::cout << "n: " << n << std::endl
-              << "n expected: " << (int)(1 / eps - 1) << std::endl;
-    // n > 1 / eps - 1
+              << "n expected: " << (int)(1 / eps - 1 + 1) << std::endl;
+    // n >= 1 / eps - 1 + 1
     return 0;
 }
