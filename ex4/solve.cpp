6fbb46baefd82a0b24df436e2b2a3d049906b1ba
Wende Tan <twd2@163.com>
2018-05-01 19:24:56 +0800

    long double, report

--- ex4/solve.cpp
+++ ex4/solve.cpp
@@ -1,12 +1,14 @@
+#include <cstdlib>
 #include <cmath>
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <chrono>
 
 class matrix
 {
 private:
-    std::vector<double> _v;
+    std::vector<long double> _v;
     int _row, _col;
 public:
     matrix(const matrix &m)
@@ -49,13 +51,13 @@ public:
 
     }
 
-    matrix(int row, int col, const std::vector<double> &v)
+    matrix(int row, int col, const std::vector<long double> &v)
         : _v(v), _row(row), _col(col)
     {
 
     }
 
-    matrix(int row, int col, std::vector<double> &&v)
+    matrix(int row, int col, std::vector<long double> &&v)
         : _v(std::move(v)), _row(row), _col(col)
     {
 
@@ -68,19 +70,19 @@ public:
         {
             for (int j = 0; j < _col; ++j)
             {
-                std::cout << _v[k] << " ";
+                std::cout << (double)_v[k] << " ";
                 ++k;
             }
             std::cout << std::endl;
         }
     }
 
-    double &operator()(int i, int j)
+    long double &operator()(int i, int j)
     {
         return _v[i * _col + j];
     }
 
-    double operator()(int i, int j) const
+    long double operator()(int i, int j) const
     {
         return _v[i * _col + j];
     }
@@ -109,7 +111,7 @@ public:
         {
             for (int j = 0; j < c._col; ++j)
             {
-                double sum = 0.0;
+                long double sum = 0.0;
                 for (int k = 0; k < _col; ++k)
                 {
                     sum += a(i, k) * b(k, j);
@@ -184,7 +186,7 @@ public:
         {
             for (int j = r; j < n; ++j)
             {
-                double sum = 0.0;
+                long double sum = 0.0;
                 for (int k = 0; k < r; ++k)
                 {
                     sum += l(r, k) * u(k, j);
@@ -193,7 +195,7 @@ public:
             }
             for (int i = r + 1; i < n; ++i)
             {
-                double sum = 0.0;
+                long double sum = 0.0;
                 for (int k = 0; k < r; ++k)
                 {
                     sum += l(i, k) * u(k, r);
@@ -201,6 +203,39 @@ public:
                 l(i, r) = (a(i, r) - sum) / u(r, r);
             }
         }
+    }
+
+    matrix sqrt() const
+    {
+        if (_row != _col)
+        {
+            exit(1);
+        }
+
+        const int n = _row;
+        const matrix &a = *this;
+        matrix l = zeros(n);
+
+        for (int j = 0; j < n; ++j)
+        {
+            long double sum = 0.0;
+            for (int k = 0; k < j; ++k)
+            {
+                sum += l(j, k) * l(j, k);
+            }
+            l(j, j) = a(j, j) - sum; // FIXME! std::sqrt(a(j, j) - sum);
+
+            for (int i = j + 1; i < n; ++i)
+            {
+                sum = 0.0;
+                for (int k = 0; k < j; ++k)
+                {
+                    sum += l(i, k) * l(j, k);
+                }
+                l(i, j) = (a(i, j) - sum) / l(j, j);
+            }
+        }
+        return l;
     }
 
     matrix solve_as_l(const matrix &b)
@@ -226,12 +261,12 @@ public:
         y._v[0] = b._v[0];
         for (int i = 1; i < n; ++i)
         {
-            double sum = 0.0;
+            long double sum = 0.0;
             for (int k = 0; k < i; ++k)
             {
                 sum += l(i, k) * y._v[k];
             }
-            y._v[i] = b._v[i] - sum;
+            y._v[i] = (b._v[i] - sum) / l(i, i);
         }
         return y;
     }
@@ -259,7 +294,7 @@ public:
         x._v[n - 1] = y._v[n - 1] / u(n - 1, n - 1);
         for (int i = n - 2; i >= 0; --i)
         {
-            double sum = 0.0;
+            long double sum = 0.0;
             for (int k = i + 1; k < n; ++k)
             {
                 sum += u(i, k) * x._v[k];
@@ -269,12 +304,45 @@ public:
         return x;
     }
 
-    double norm_inf() const
+    matrix solve_as_lt(const matrix &y)
     {
-        double norm = _v[0];
+        if (_row != _col)
+        {
+            exit(1);
+        }
+
+        if (y._row != _row)
+        {
+            exit(1);
+        }
+
+        if (y._col != 1)
+        {
+            exit(1);
+        }
+
+        const int n = _row;
+        const matrix &l = *this;
+        matrix x(n, 1);
+        x._v[n - 1] = y._v[n - 1] / l(n - 1, n - 1);
+        for (int i = n - 2; i >= 0; --i)
+        {
+            long double sum = 0.0;
+            for (int k = i + 1; k < n; ++k)
+            {
+                sum += l(k, i) * x._v[k];
+            }
+            x._v[i] = (y._v[i] - sum) / l(i, i);
+        }
+        return x;
+    }
+
+    long double norm_inf() const
+    {
+        long double norm = _v[0];
         for (int i = 0; i < _row; ++i)
         {
-            double sum = 0.0;
+            long double sum = 0.0;
             for (int j = 0; j < _col; ++j)
             {
                 sum += std::abs((*this)(i, j));
@@ -287,9 +355,22 @@ public:
         return norm;
     }
 
-    double norm_row() const
+    long double norm_row() const
     {
         return norm_inf();
+    }
+
+    matrix transpose() const
+    {
+        matrix result(_col, _row);
+        for (int i = 0; i < _row; ++i)
+        {
+            for (int j = 0; j < _col; ++j)
+            {
+                result(j, i) = (*this)(i, j);
+            }
+        }
+        return result;
     }
 
     matrix inverse() const
@@ -326,7 +407,7 @@ public:
         return inv;
     }
 
-    double cond_inf() const
+    long double cond_inf() const
     {
         matrix inv = inverse();
         return inv.norm_inf() * norm_inf();
@@ -365,51 +446,164 @@ matrix matrix::ident(int n)
     return m;
 }
 
-int main()
+void cond_test()
 {
     // (1)
-    std::cout << std::fixed << std::setprecision(20);
-    std::cout << "cond(H3) = " << matrix::hilbert(3).cond_inf() << std::endl;
-    std::cout << "cond(H4) = " << matrix::hilbert(4).cond_inf() << std::endl;
-    // (2)
-    const int n = 100;
-    std::cout << "cond(H" << n << ") = " << matrix::hilbert(n).cond_inf() << std::endl;
-    std::cout << std::fixed << std::setprecision(6);
-    std::cout << "H" << n << " = " << std::endl;
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "cond(H3)_inf = " << (double)matrix::hilbert(3).cond_inf() << std::endl;
+    std::cout << "cond(H4)_inf = " << (double)matrix::hilbert(4).cond_inf() << std::endl;
+}
+
+void lu_test(const int n)
+{
+    // (2), (4), (5)
+    std::cout << "========== LU ==========" << std::endl;
     matrix h = matrix::hilbert(n);
-    h.print();
-    std::cout << std::endl;
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "cond(H" << n << ")_inf = " << (double)h.cond_inf() << std::endl;
+    std::cout << std::fixed << std::setprecision(6);
+    std::cout << "H" << n << " = " << std::endl; h.print(); std::cout << std::endl;
+    // LU分解
     matrix l, u;
     h.lu(l, u);
-    std::cout << "L = " << std::endl;
-    l.print();
-    std::cout << std::endl;
-    std::cout << "U = " << std::endl;
-    u.print();
-    std::cout << std::endl;
-    std::cout << "L * U = " << std::endl;
-    (l * u).print();
-    std::cout << std::endl;
-    matrix x(n, 1); // 精确解
+    std::cout << "L = " << std::endl; l.print(); std::cout << std::endl;
+    std::cout << "U = " << std::endl; u.print(); std::cout << std::endl;
+    // 验证
+    std::cout << "L * U = " << std::endl; (l * u).print(); std::cout << std::endl;
+    matrix x(n, 1); // 构造精确解
     for (int i = 0; i < n; ++i)
     {
         x(i, 0) = 1.0;
     }
-    std::cout << "x = " << std::endl;
-    x.print();
-    std::cout << std::endl;
-    matrix b = h * x;
-    std::cout << "b = h * x = " << std::endl;
-    b.print();
-    std::cout << std::endl;
-    matrix x_hat = u.solve_as_u(l.solve_as_l(b));
-    std::cout << "x_hat = " << std::endl;
-    x_hat.print();
-    std::cout << std::endl;
-    matrix r = b - h * x_hat,
-           delta = x_hat - x;
     std::cout << std::fixed << std::setprecision(20);
-    std::cout << "norm_inf(r) = " << r.norm_inf() << std::endl;
-    std::cout << "norm_inf(delta) = " << delta.norm_inf() << std::endl;
+    std::cout << "x = " << std::endl; x.print(); std::cout << std::endl;
+    matrix b = h * x;
+    std::cout << "b = H" << n << " * x = " << std::endl; b.print(); std::cout << std::endl;
+    // 用LU分解结果计算近似解\hat x
+    matrix x_hat = u.solve_as_u(l.solve_as_l(b));
+    std::cout << "x_hat = " << std::endl; x_hat.print(); std::cout << std::endl;
+    matrix r = b - h * x_hat, delta = x_hat - x;
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "norm_inf(r) = " << (double)r.norm_inf() << std::endl;
+    std::cout << "norm_inf(delta) = " << (double)delta.norm_inf() << std::endl;
+    std::cout << "Applying 1e-7 jitter..." << std::endl;
+    matrix b1 = b;
+    b1(6, 0) += 1e-7;
+    std::cout << std::fixed << std::setprecision(20);
+    std::cout << "b1 = " << std::endl; b1.print(); std::cout << std::endl;
+    matrix x1_hat = u.solve_as_u(l.solve_as_l(b1));
+    std::cout << "x1_hat = " << std::endl; x1_hat.print(); std::cout << std::endl;
+    matrix r1 = b1 - h * x1_hat, delta1 = x1_hat - x;
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "norm_inf(r1) = " << (double)r1.norm_inf() << std::endl;
+    std::cout << "norm_inf(delta1) = " << (double)delta1.norm_inf() << std::endl;
+}
+
+void sqrt_test(const int n)
+{
+    // (3)
+    std::cout << "========== Cholesky ==========" << std::endl;
+    matrix h = matrix::hilbert(n);
+    std::cout << std::fixed << std::setprecision(6);
+    // Cholesky分解
+    matrix l = h.sqrt();
+    std::cout << "L = " << std::endl; l.print(); std::cout << std::endl;
+    // 验证
+    std::cout << "L * LT = " << std::endl; (l * l.transpose()).print(); std::cout << std::endl;
+    matrix x(n, 1); // 构造精确解
+    for (int i = 0; i < n; ++i)
+    {
+        x(i, 0) = 1.0;
+    }
+    std::cout << std::fixed << std::setprecision(20);
+    std::cout << "x = " << std::endl; x.print(); std::cout << std::endl;
+    matrix b = h * x;
+    std::cout << "b = H" << n << " * x = " << std::endl; b.print(); std::cout << std::endl;
+    // 用LU分解结果计算近似解\hat x
+    matrix x_hat = l.solve_as_lt(l.solve_as_l(b));
+    std::cout << "x_hat = " << std::endl; x_hat.print(); std::cout << std::endl;
+    matrix r = b - h * x_hat, delta = x_hat - x;
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "norm_inf(r) = " << (double)r.norm_inf() << std::endl;
+    std::cout << "norm_inf(delta) = " << (double)delta.norm_inf() << std::endl;
+}
+
+void benchmark(const int n, const int iter = 10000)
+{
+    using namespace std::chrono;
+    std::cout << "========== benchmark ==========" << std::endl;
+    matrix h = matrix::hilbert(n);
+    matrix x(n, 1); // 构造精确解
+    for (int i = 0; i < n; ++i)
+    {
+        x(i, 0) = 1.0;
+    }
+    matrix b = h * x;
+
+	steady_clock::time_point start_time = steady_clock::now();
+    steady_clock::time_point end_time = steady_clock::now();
+    double bias = duration_cast<duration<double> >(end_time - start_time).count();
+
+    double lu_time;
+    start_time = steady_clock::now();
+    for (int i = 0; i < iter; ++i)
+    {
+        matrix l, u;
+        h.lu(l, u);
+        matrix x_hat = u.solve_as_u(l.solve_as_l(b));
+    }
+    end_time = steady_clock::now();
+    lu_time = duration_cast<duration<double> >(end_time - start_time).count() - bias;
+    lu_time /= iter;
+
+    double sqrt_time;
+    start_time = steady_clock::now();
+    for (int i = 0; i < iter; ++i)
+    {
+        matrix l = h.sqrt();
+        matrix x_hat = l.solve_as_lt(l.solve_as_l(b));
+    }
+    end_time = steady_clock::now();
+    sqrt_time = duration_cast<duration<double> >(end_time - start_time).count() - bias;
+    sqrt_time /= iter;
+
+    std::cout << "n = " << n << std::endl;
+    std::cout << std::fixed << std::setprecision(2);
+    std::cout << "LU time = " << lu_time * 1e9 << " ns" << std::endl;
+    std::cout << "Cholesky time = " << sqrt_time * 1e9 << " ns" << std::endl;
+}
+
+void n_test(const int n)
+{
+    std::cout << std::scientific << std::setprecision(10);
+    std::cout << "n = " << n << std::endl;
+    matrix h = matrix::hilbert(n);
+    // LU分解
+    matrix l, u;
+    h.lu(l, u);
+    matrix x(n, 1); // 构造精确解
+    for (int i = 0; i < n; ++i)
+    {
+        x(i, 0) = 1.0;
+    }
+    matrix b = h * x;
+    // 用LU分解结果计算近似解\hat x
+    matrix x_hat = u.solve_as_u(l.solve_as_l(b));
+    matrix r = b - h * x_hat, delta = x_hat - x;
+    std::cout << "norm_inf(r) = " << (double)r.norm_inf() << std::endl;
+    std::cout << "norm_inf(delta) = " << (double)delta.norm_inf() << std::endl;
+}
+
+int main()
+{
+    cond_test();
+    lu_test(10);
+    sqrt_test(10);
+    benchmark(100, 1000);
+    std::cout << "========== n ==========" << std::endl;
+    for (int n = 1; n <= 100; ++n)
+    {
+        n_test(n);
+    }
     return 0;
 }
